7e903150772fd2b1fcf550e711d656cf0752f26b
Alexandr Lomash <lomash@MacBook-Pro-Alexandr-Lomash.local>
2026-07-03 12:25:18 +0300

    update weights

--- src/model.py
+++ src/model.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 from torchvision import models
 
 def get_model(num_classes):
-    model = models.resnet18(pretrained=True)
+    model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
 
     # замена классификатора
     model.fc = nn.Linear(model.fc.in_features, num_classes)
